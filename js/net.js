3395b3cc77c3a28d594929ffea012167e3de1e51
gabrielavalos <gabby.avalos16@gmail.com>
2021-08-31 17:12:22 -0500

    set log scale, removed all 0s from data

--- js/net.js
+++ js/net.js
@@ -2,7 +2,7 @@ function createPartnerOptions() {
     var partnerSelector = d3.select("#partner-name"); //SELECT <select> WHERE PARTNER NAMES WILL APPEAR
     d3.json('./static/partner_id.json').then((partners) => { //READ IN JSON FILE COINTAING ALL PARTNER'S NAMES
         //console.log(partners);
-        var southTexasPartners = Object.values(partners.partner_name) //EMPTY ARRAY TO CONTAIN ALL PARTNER'S NAME (REPEATED)
+        var southTexasPartners = Object.values(partners.partner_name).sort() //EMPTY ARRAY TO CONTAIN ALL PARTNER'S NAME (REPEATED)
         //console.log(southTexasPartners);
         southTexasPartners.forEach((partner) =>{
             //console.log(partner)
@@ -33,11 +33,12 @@ function createCurves() {
         var oil =[];
         
         oilOwnership.forEach((oilDay) => { //LOOP THROUGH  ROW OF DATA (interest)
-            //console.log(oilDay);
-            if (oilDay.hasOwnProperty(values)) {
+           // console.log(oilDay);
+            if (oilDay.hasOwnProperty(values) & oilDay[values] > 0)  {
                
                 dates.push(oilDay.Date)
                 oil.push(oilDay[values]) //THIS DOES NOT WORK
+                //console.log(oilDay[values])
             }; 
          }); //CLOSE interest LOOP
          //console.log(dates)
@@ -46,71 +47,73 @@ function createCurves() {
          d3.json('./static/ownership-gas.json').then((gasOwnership) => {
            
             var gas = [];
+            var gasDates = []
             gasOwnership.forEach((gasDay) => { //LOOP THROUGH  ROW OF DATA (interest)
                 //console.log(oilDay);
-                if (gasDay.hasOwnProperty(values)) {
+                if (gasDay.hasOwnProperty(values) & gasDay[values] > 0) {
                    
                   
-                    gas.push(gasDay[values]) //THIS DOES NOT WORK
+                    gas.push(gasDay[values]);
+                    gasDates.push(gasDay.Date)
                 }; 
              }); //CLOSE interest LOOP
+             var mostRecentEntry = dates[dates.length-1]; //MOST RECENT DATE WITHOUT HOUR AS VARIABLE
+             var mostRecentDate = new Date(mostRecentEntry) //MAKE MOST RECENT ENTRY A DATE 
+             var nextYearsDate = new Date(mostRecentDate.setFullYear(mostRecentDate.getFullYear() + 1)); //GET YEAR FROM MOST RECENT DATE AND ADD A YEAR
+             var nextYear= nextYearsDate.getFullYear() //GET NEXT YEARS DATE
+             var nextMonth= nextYearsDate.getMonth() + 1 // GET NEXTS YEARS MONTH, ADD ONE BECAUSE MONTHS ARE INDEXED AT 0
+             var nextDate= nextYearsDate.getDate() //GET NEXT YEARS DATE
+             nextYearGraph = `${nextYear}-${nextMonth}-${nextDate}`; // CREATE FULL DATE FOR NEXT YEAR IN RIGHT FORMAT FOR AXIS
+             console.log(`${nextYearGraph} is a year from the most recent production date.`);
 
-             //console.log(gas);
-             //console.log(dates);
-           
-            
-
-
-
-         var mostRecentEntry = dates[0]; //MOST RECENT DATE WITHOUT HOUR AS VARIABLE
-           // console.log("mostRecentEntry", mostRecentEntry);
-            var mostRecentDate = new Date(mostRecentEntry) //MAKE MOST RECENT ENTRY A DATE
-            //var mostRecentDate = new Date(nextYear); //MAKE VARIABLE INTO DATE
-            //console.log("mostRecentDate", mostRecentDate);
-            var nextYearsDate = new Date(mostRecentDate.setFullYear(mostRecentDate.getFullYear() + 1)); //GET YEAR FROM MOST RECENT DATE AND ADD A YEAR
-            var nextYear= nextYearsDate.getFullYear() //GET NEXT YEARS DATE
-            var nextMonth= nextYearsDate.getMonth() + 1 // GET NEXTS YEARS MONTH, ADD ONE BECAUSE MONTHS ARE INDEXED AT 0
-            var nextDate= nextYearsDate.getDate() //GET NEXT YEARS DATE
-            nextYearGraph = `${nextYear}/${nextMonth}/${nextDate}`; // CREATE FULL DATE FOR NEXT YEAR IN RIGHT FORMAT FOR AXIS
-           // console.log(`${nextYearGraph} is a year from the most recent production date. This is from curvesHome()`);
-  
-            var dataOil = [{
-                x: dates,
-                y: oil,
-                type: "line",
-                line:
-                    {color: "green"}}];
-            var layoutOil = {
-                title: "Oil BBL",
-                yaxis: {
-                    title: "BOPD Net",
-                    // type: 'log',
-                     //autorange: true
-                    },
-                 //xaxis: {
-                     //autorange: false,
-                     //range: [dates[dates.length-1], nextYearGraph]
-                // }
-            };
-  
+             console.log(gas);
+             console.log(dates);
+             console.log(oil);
+             console.log(gasDates)
+             
+             var dataOil = [{
+                 x: dates,
+                 y: oil,
+                 type: "line",
+                 line:
+                 {color: "green"}
+                }
+            ];   
+             var layoutOil = {
+                 title: "Oil BBL",
+                 yaxis: {
+                     title: "BOPD Net",
+                     zeroline: true,
+                     showline: true,
+                     type: 'log',
+                     autorange: true},
+                 xaxis: {
+                     autorange: false,
+                     range: [dates[dates.length - 365], nextYearGraph] //365 to only show 1 year back, can make it into a variable, include an Inception button that does it from dates[0]
+                    }
+                };
             Plotly.newPlot("oilDeclineCurve", dataOil, layoutOil);
 
             var dataGas = [{
-                x: dates,
+                x: gasDates,
                 y: gas,
                 type: "line",
                 line:
-                    {color: "red"}}];
+                    {color: "red"}
+                }
+            ];
             var layoutGas = {
                 title: "Gas BBL",
                 yaxis: {
                     title: "MCFD Net",
-                    // type: 'log',
-                    // autorange: true
+                    zeroline: true,
+                     showline: true,
+                     type: 'log',
+                     autorange: true
                 },
-                // xaxis: {
-                //     autorange: false,
-                //     range: [dates[dates.length-1], nextYearGraph]}
+                xaxis: {
+                     autorange: false,
+                    range: [gasDates[0], nextYearGraph]}
                 };
             Plotly.newPlot("gasDeclineCurve", dataGas, layoutGas);
 
